baf7d24c4f13d1574a9c6d0a0cb57d74619912b2
hekaahve <heini.k.ahven@student.jyu.fi>
2021-03-01 15:14:29 +0200

    1.6

--- teht1.6-1.14/unicafe/src/index.js
+++ teht1.6-1.14/unicafe/src/index.js
@@ -1,15 +1,47 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = () =>{
+  return <h1>Give feedback</h1>
+}
+const Header2 = () =>{
+  return <h1>statistics</h1>
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+  
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () =>{
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () =>{
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () =>{
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Header/>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
+      <Header2/>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
