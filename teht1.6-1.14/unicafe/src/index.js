e731fe144d7efdc403fc0043c41599e39a2d0508
hekaahve <heini.k.ahven@student.jyu.fi>
2021-03-02 22:17:34 +0200

    1.7: unicafe step2

--- teht1.6-1.14/unicafe/src/index.js
+++ teht1.6-1.14/unicafe/src/index.js
@@ -32,16 +32,21 @@ const App = () => {
     setBad(bad + 1)
   }
 
+
+
   return (
     <div>
       <Header/>
-      <button onClick={handleGood}>Good</button>
-      <button onClick={handleNeutral}>Neutral</button>
-      <button onClick={handleBad}>Bad</button>
+      <Button onClick={handleGood} text='Good'/>
+      <Button onClick={handleNeutral} text='Neutral'/>
+      <Button onClick={handleBad} text='Bad'/>
       <Header2/>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All:{(good+bad+neutral)}</p>
+      <p>Average: {(good-bad)/(good+bad+neutral)}</p>
+      <p>Positive: {(100*good/(good+bad+neutral))} %</p>
     </div>
   )
 }
