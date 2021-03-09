aade5eec2e5e560d54ce6350243ba8ecf526cf3e
hekaahve <heini.k.ahven@student.jyu.fi>
2021-03-09 22:03:35 +0200

    1.12* anekdootit step1

---
+++ teht1.6-1.14/anekdootit/src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './App.css';
+
+const Button = (props) => (
+  <div>
+    <button onClick={props.handleClick} type ="button">{props.text}</button>
+  </div>
+  
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleSelected = () =>{
+     //sekoita anekdootit
+     setSelected(Math.floor(Math.random() * 6)) 
+  }
+  return (
+    <div>
+      {anecdotes[selected]}
+      <Button handleClick={handleSelected} text="next anekdote"/>
+    </div>
+  )
+}
+
+export default App
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
