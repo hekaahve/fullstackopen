b79175f6b0f925e1058a7ee90aec9ae6b5bd74b0
hekaahve <heini.k.ahven@student.jyu.fi>
2021-03-11 13:55:09 +0200

    1.13* anekdootit step2

--- teht1.6-1.14/anekdootit/src/index.js
+++ teht1.6-1.14/anekdootit/src/index.js
@@ -6,7 +6,6 @@ const Button = (props) => (
   <div>
     <button onClick={props.handleClick} type ="button">{props.text}</button>
   </div>
-  
 )
 
 const App = () => {
@@ -19,16 +18,31 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
    
+  //anekdootti-taulukon paikat numeroin
   const [selected, setSelected] = useState(0)
+
+  //luodaan taulukko äänestykselle
+  var points = Array(6).fill(0)
+  const [votes, setVotes] = useState(points)
 
   const handleSelected = () =>{
      //sekoita anekdootit
      setSelected(Math.floor(Math.random() * 6)) 
   }
+
+  const handleVote =() => {
+    const copy = {...votes} //kopioidaan votes uuteen taulukkoon
+    copy[selected] = copy[selected]+1 //asetetaan valittu anekdootti +1
+    setVotes(copy) //asetetaan votes kopio-taulukon arvoilla
+  }
+
+
   return (
     <div>
-      {anecdotes[selected]}
-      <Button handleClick={handleSelected} text="next anekdote"/>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick = {handleSelected} text="Next anekdote"/>
+      <Button handleClick = {handleVote} text = "Vote"/> 
     </div>
   )
 }
